7c61590bbab7d95182368fb22e43cb14c02fafee
Timuçin Alpay Bohur <bohuralpay@gmail.com>
2023-09-23 19:50:42 +0300

    teslim

--- frontend/App.js
+++ frontend/App.js
@@ -3,23 +3,23 @@ import axios from "axios";
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
+import { useGeceModu } from "./hooks/useGeceModu";
+import { useAxios } from "./hooks/useAxios";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-  const [geceModu, setGeceModu] = useState(false);
+  const [geceModu, setGeceModu] = useGeceModu(false);
+  const [coinData, err, loading, getData] = useAxios(
+    "get",
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+  );
 
   useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+    getData();
   }, []);
   return (
     <div className={geceModu ? "dark-mode App" : "App"}>
       <Navbar geceModu={geceModu} setGeceModu={setGeceModu} />
-      <Charts coinData={coinData} />
+      {loading ? "loading" : <Charts coinData={coinData} />}
     </div>
   );
 };
